daec0d8b30c2b1b143ba2263fa0dac3a7b2c4440
The-Keeper <6674932+The-Keeper@users.noreply.github.com>
2023-07-05 15:28:49 +0500

    traits done

--- exercises/traits/traits2.rs
+++ exercises/traits/traits2.rs
@@ -16,10 +16,12 @@ trait AppendBar {
     fn append_bar(self) -> Self;
 }
 
-//TODO: Add your code here
-
-
-
+impl AppendBar for Vec<String> {
+    fn append_bar(mut self) -> Vec<String> {
+        self.push(String::from("Bar"));
+        self
+    }
+}
 
 #[cfg(test)]
 mod tests {
